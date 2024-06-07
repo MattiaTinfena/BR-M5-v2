2e8710a519279f77b29899ef4d0526359a9bb347
Mattia <mattia.tinfena@gmail.com>
2024-06-07 11:15:02 +0200

    Added main menu with Timelapse and Bulb mode📸⏱️

--- lib/Display/Display.cpp
+++ lib/Display/Display.cpp
@@ -1,6 +1,7 @@
 #include "Display.h"
 #include <Arduino.h>
 #include "M5StickCPlus2.h"
+#include "cplus_RTC.h"
 
 Display::Display(M5GFX* tft, String name_remote):
     buffer(tft),
@@ -34,7 +35,7 @@ void Display::set_address(String address_str)
     address.toUpperCase();
 }
 
-void Display::set_main_menu_screen(int delay, String status, unsigned int pic_count)
+void Display::set_main_menu_screen(unsigned int delay, String status, unsigned int pic_count, unsigned long time, int mode)
 {
     buffer.fillRect(0, 0, 240, 135, (negatif?TFT_BLACK:TFT_WHITE));
 
@@ -51,18 +52,119 @@ void Display::set_main_menu_screen(int delay, String status, unsigned int pic_co
 
     buffer.drawFloat(battery_level(),0, 240 - buffer.textWidth((String)(int)battery_level()) - buffer.textWidth("%") -4 ,5); 
     buffer.drawString("%", 240- buffer.textWidth("%") - 2, 5);
+    if (delay == -1){
 
-    buffer.drawString("Pic:",180 + 10 - (buffer.textWidth("Pic:")/2),35);
+        buffer.drawString("Press button", 120 - (buffer.textWidth("Press button")/2.0), 40 );
+        buffer.drawString("-->", 220 - buffer.textWidth("-->"), 55 );
+        buffer.drawString("to connect", 120 - (buffer.textWidth("to connect")/2.0), 70 );
+    }
+    else{
+        if (mode == 0){
+            // setting mode
 
-    buffer.drawString("Interval (s):", 60 + 10 - (buffer.textWidth("Interval (s):")/2), 35);
+            String mode_status ="";
+            switch (delay)
+            {
+            case 1:
+                mode_status = "Single shoot";
+                break;
+            case 2:
+                mode_status = "Timelaps";
+                break;
+            case 3:
+                mode_status = "Startrail";
+                break;
+            case 4:
+                mode_status = "Bulb";
+                break;
+            default:
+            mode_status = "";
+                break;
+            }
 
-    buffer.setFreeFont(font_titles);
+            buffer.setFreeFont(font_titles);
 
-    buffer.drawFloat(float(delay)/1000.0, 1, 30 + 10, 60);
-    buffer.drawFloat(pic_count,0,180 + 10 - (buffer.textWidth((String)(int)pic_count))/2,60); 
+            buffer.drawString(mode_status, 120 - (buffer.textWidth(mode_status)/2.0), 50 );
+        }
 
-    buffer.pushSprite(0,0);
-}
+        else if (mode == 1){    // single shot 
+            buffer.setFreeFont(font_text);
+
+            buffer.drawString("Pic:",120 - (buffer.textWidth("Pic:")/2),35);
+
+            buffer.setFreeFont(font_titles);
+
+            buffer.drawFloat(pic_count,0,120 - (buffer.textWidth((String)(int)pic_count))/2,60); 
+        } 
+
+        else if (mode == 2) {   //Timelaps
+
+
+            buffer.setFreeFont(font_text);
+
+            buffer.drawString("Pic:",180 + 10 - (buffer.textWidth("Pic:")/2),35);
+
+            buffer.drawString("Interval(s):", 60 + 10 - (buffer.textWidth("Interval(s):")/2), 35);
+
+            buffer.setFreeFont(font_titles);
+
+            buffer.drawFloat(float(delay), 0, 30 + 10, 60);
+            buffer.drawFloat(pic_count,0,180 + 10 - (buffer.textWidth((String)(int)pic_count))/2,60); 
+        }
+
+        else if (mode == 3) {   // Startrail
+
+
+            buffer.setFreeFont(font_text);
+
+            buffer.drawString("Pic:",180 + 10 - (buffer.textWidth("Pic:")/2),35);
+
+            buffer.drawString("Shutter speed:", 60 + 10 - (buffer.textWidth("Shutter speed:")/2), 35);
+
+            buffer.setFreeFont(font_titles);
+
+            buffer.drawFloat(float(delay), 0, 30 + 10, 60);
+            buffer.drawFloat(pic_count,0,180 + 10 - (buffer.textWidth((String)(int)pic_count))/2,60); 
+        }
+
+        else if (mode == 4){    //Bulb   
+
+             if(status != "Shooting"){
+                
+                buffer.setFreeFont(font_text);
+
+                buffer.drawString("Set the interval:",120 - (buffer.textWidth("Set the interval:")/2),35);
+
+                buffer.setFreeFont(font_titles);
+                float minutes;
+                float sec = modf((delay/60), &minutes);
+                sec = delay - (minutes*60);
+                buffer.drawFloat(minutes, 0, 90 - (buffer.textWidth((String)(int)minutes))/2 , 60);
+                buffer.drawString(":",120 - (buffer.textWidth(":")/2),55);
+                buffer.drawFloat(sec,0,115 + (buffer.textWidth((String)(int)10))/2,60); 
+            }
+
+            else if(status == "Shooting"){
+                
+                buffer.setFreeFont(font_text);
+
+                buffer.drawString("Remaining time:",120 - (buffer.textWidth("Remaining time:")/2),35);
+
+                buffer.setFreeFont(font_titles);
+                float remining_time = delay - time;
+                float remining_minutes;
+                float remining_seconds = modf((remining_time/60), &remining_minutes);
+                remining_seconds = remining_time - (remining_minutes*60);
+                
+                
+                buffer.drawFloat(remining_minutes, 0, 90 - (buffer.textWidth((String)(int)remining_minutes))/2 , 60);
+                buffer.drawString(":",120 - (buffer.textWidth(":")/2),55);
+                buffer.drawFloat(remining_seconds,0,115 + (buffer.textWidth((String)(int)10))/2,60); 
+            }
+        } 
+    }
+    buffer.pushSprite(0,0);        
+    }
 
 float Display::battery_level(void){
     float mv = (float)StickCP2.Power.getBatteryVoltage();
@@ -71,9 +173,9 @@ float Display::battery_level(void){
         v = 4.2;
     }
     float level = ((v-3.27)/(4.2-3.27))*100;
-    float bl = 0.7*battery_lvl+0.3*level;
+    float bl = 0.8*battery_lvl+0.2*level;
 
-    if ((bl > (battery_lvl + 0.5)) || (bl < (battery_lvl - 0.5))) {
+    if ((bl > (battery_lvl + 0.6)) || (bl < (battery_lvl - 0.6))) {
         battery_lvl = bl;
     }
     return roundf(battery_lvl);
